53c3cb46cff30c01af058711def6388610b937ea
Krishnamoorthy.S <s.krishnasundar95@gmail.com>
2018-03-01 12:19:31 +0530

    Added Buyer GST addition

--- src/main/resources/static/getBuyerController.js
+++ src/main/resources/static/getBuyerController.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         url: "http://localhost:8080/buyers"
     }).then(function(data) {
        	for(var i=0;i<=data.length;i++){
-       		$('.products').append("<tr><td>"+data[i].buyerId+"</td><td>HELLO </td></tr>");
+       		$('.products').append("<tr><td>"+data[i].buyerId+"</td><td>"+data[i].buyerName+"</td><td>"+data[i].buyerGst+"</td></tr>");
     	}
     });
 });
