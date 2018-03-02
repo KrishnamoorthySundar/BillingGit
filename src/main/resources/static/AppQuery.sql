bc6fb24418a4534f78fb08985c2479a18311796b
Krishnamoorthy.S <s.krishnasundar95@gmail.com>
2018-03-02 15:56:24 +0530

    Implemented automatic date selection

---
+++ src/main/resources/static/AppQuery.sql
@@ -0,0 +1,3 @@
+create table BUYER (buyer_id int(10) primary key,buyer_gst varchar(255),buyer_name varchar(255));
+create table PRODUCT (product_id int(10) primary key,product_name varchar(255));
+create table USER (user_name varchar(255) primary key,user_password varchar(255));
